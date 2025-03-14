b7033610b297c574ee19f0bbc6150d0c91d34002
George Yu <gyu13@illinois.edu>
2025-03-14 15:37:52 +0000

    a

--- src/solution.cc
+++ src/solution.cc
@@ -5,9 +5,16 @@ std::vector<std::pair<unsigned int, unsigned int>> SolveMaze(
     unsigned int start_x,
     unsigned int start_y) {
   std::vector<std::pair<unsigned int, unsigned int>> path;
-  std::vector<std::vector<bool>> visited;
-
-  return path;
+  std::vector<std::vector<bool>> visited = std::vector<std::vector<bool>>(
+      maze.size(), std::vector<bool>(maze[0].size(), false));
+  unsigned int i = start_x;
+  unsigned int j = start_y;
+  if (SolveMazeRecursive(maze, i, j, path, visited)) {
+    path.push_back(std::make_pair(i, j));
+  };
+  if (maze[i][j] == 'X') {
+    return path;
+  }
 }
 
 bool SolveMazeRecursive(
